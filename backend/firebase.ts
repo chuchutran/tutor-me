459b3d02b691c50624899d49118a25739ac2c95e
Jasmine Dong <jasminedong@dhcp-vl2041-24405.eduroam.cornell.edu>
2024-04-21 20:49:11 -0400

    set up firebase

---
+++ backend/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import serviceAccount from "./service_account.json";
+
+const app = initializeApp({
+    credential: cert({
+      clientEmail: serviceAccount.client_email,
+      privateKey: serviceAccount.private_key,
+      projectId: serviceAccount.project_id,
+    }),
+  });
+const db = getFirestore();
+
+export { db };
+
+/**import { db } from "./firebase";```*/
