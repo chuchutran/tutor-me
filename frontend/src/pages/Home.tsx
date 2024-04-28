33ac907898d3ab4cb392f524f71e8024da67ac31
Kelly Tran <kt457@cornell.edu>
2024-04-27 21:22:54 -0400

    Created SearchPosts component for search bar

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,9 +2,9 @@ import './Home.css';
 
 const HomePage = () => (
     <div id='home-page' className='hero'>
-        <h1 style={{ fontSize: "4rem", marginTop: "5rem" }}>Tutor Me</h1>
-        <div>Student tutoring platform for Cornell Students</div>
-        <div style={{ fontSize: "2rem" }}>What class do you need help with?</div>
+        <h1 style={{ fontSize: "4rem", marginTop: "5rem", marginBottom: "0" }}>Tutor Me</h1>
+        <div style={{ marginTop: "0" }}>Student tutoring platform for Cornell Students</div>
+        <div style={{ fontSize: "2rem", marginTop: "4rem" }}>What class do you need help with?</div>
 
     </div>
 );
