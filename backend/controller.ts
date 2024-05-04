b9e735b5a1bd924e9534f195c61c755f69e606c8
Vicky Wang <vw92@cornell.edu>
2024-05-03 22:58:53 -0400

    Created endpoint for adding profile and deleting post

--- backend/controller.ts
+++ backend/controller.ts
@@ -14,3 +14,26 @@ export const addPost = async (post: Post) => {
     const docRef = await db.collection('posts').add(post);
     return docRef.id;
 };
+
+export const updateProfile = async (userid: string, profileUrl: string) => {
+    const docRef = db.collection('users').doc(userid);
+
+    try {
+        await docRef.update({
+            profileUrl: profileUrl
+        });
+    } catch (error) {
+        console.error("Error updating profile:", error);
+        throw error;
+    }
+};
+
+export const deletePost = async (postid: string) => {
+    const docRef = db.collection('posts').doc(postid);
+
+    try {
+        await docRef.delete();
+    } catch (error) {
+        console.error("Error deleting post:", error);
+    }
+}
--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import { User } from "./types";
 import { Post } from "./types";
 import { addUser, addPost } from "./controller";
+import { updateProfile } from "./controller";
+import { deletePost } from "./controller";
 
 
 const app: Express = express();
@@ -18,65 +20,92 @@ app.use(express.json());
 // Searbar
 // Filter
 
-// POST Requests
-// Create Post
-// Create User
-
-// PUT Requests
-// Edit Profile info
-
-// DELETE Requests
-// Delete Post
-
 // POST REQUESTS
 // Create User
 app.post("/api/user", async (req, res) => {
-    console.log("[POST] entering '/user endpoint");
-    const { username, password, name, email, phone, profileUrl} = req.body;
-    const user: User = {
-      username,
-      password,
-      name,
-      email,
-      phone,
-      profileUrl
-    };
-    try {
-      const userId = await addUser(user);
-      res.status(200).send({
-        message: `SUCCESS added person with ID: ${userId} to the users collection in Firestore`,
-        userid: userId
-      });
-    } catch (err) {
-      res.status(500).json({
-        error: `ERROR: an error occurred in the /api/user endpoint: ${err}`,
-      });
-    }
-  });
+  console.log("[POST] entering '/user endpoint");
+  const { username, password, name, email, phone, profileUrl } = req.body;
+  const user: User = {
+    username,
+    password,
+    name,
+    email,
+    phone,
+    profileUrl
+  };
+  try {
+    const userId = await addUser(user);
+    res.status(200).send({
+      message: `SUCCESS added person with ID: ${userId} to the users collection in Firestore`,
+      userid: userId
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `ERROR: an error occurred in the /api/user endpoint: ${err}`,
+    });
+  }
+});
 
-  app.post("/api/post/:userid", async (req, res) => {
-    console.log("[POST] entering '/user/:userid' endpoint");
-    const userid: string = req.params.userid;
-    const { course, availabilities, description} = req.body;
-    const post: Post = {
-      userid,
-      course,
-      availabilities,
-      description
-    };
-    try {
-      const postId = await addPost(post);
-      res.status(200).send({
-        message: `SUCCESS added post with ID: ${postId} to the posts collection in Firestore`,
-        postId: postId
-      });
-    } catch (err) {
-      res.status(500).json({
-        error: `ERROR: an error occurred in the /api/post/userid endpoint: ${err}`,
-      });
-    }
-  });
+app.post("/api/post/:userid", async (req, res) => {
+  console.log("[POST] entering '/user/:userid' endpoint");
+  const userid: string = req.params.userid;
+  const { course, availabilities, description } = req.body;
+  const post: Post = {
+    userid,
+    course,
+    availabilities,
+    description
+  };
+  try {
+    const postId = await addPost(post);
+    res.status(200).send({
+      message: `SUCCESS added post with ID: ${postId} to the posts collection in Firestore`,
+      postId: postId
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `ERROR: an error occurred in the /api/post/userid endpoint: ${err}`,
+    });
+  }
+});
 
 app.listen(port, hostname, () => {
-    console.log("Listening");
+  console.log("Listening");
+});
+
+// PUT Requests
+// Edit Profile info
+app.put("/api/user/profileurl/:userid", async (req, res) => {
+  console.log("[PUT] entering '/api/user/profileUrl/:userid' endpoint");
+  const userid: string = req.params.userid;
+  const profileurl: string = req.body.profileurl
+
+  try {
+    await updateProfile(userid, profileurl);
+    res.status(200).send({
+      message: `SUCESS updated user with userid: ${userid} from user collection in Firestore`,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `ERROR: an error occured in the /api/user/:userid endpoint ${err}`,
+    });
+  }
+});
+
+// DELETE Requests
+// Delete Post
+app.delete("/api/post/delete/:postid", async (req, res) => {
+  console.log("[DELETE] entering '/api/post/delete/:postid' endpoint");
+  const postid: string = req.params.postid;
+
+  try {
+    await deletePost(postid);
+    res.status(200).send({
+      message: `SUCCESS deleted post with postid: ${postid} from the post collection in Firestore`,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `ERROR: an error occurred in the /api/post/delete/:postid endpoint: ${err}`,
+    });
+  }
 });
