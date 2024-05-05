562398c1932e867061a516a45c1fc8f09034a1fc
Dora <cyweng03@gmail.com>
2024-05-05 01:23:18 -0400

    YAY

--- backend/controller.ts
+++ backend/controller.ts
@@ -1,9 +1,11 @@
 import { User } from "./types";
 import { Post } from "./types";
 import { db } from "./firebase";
+import { getFirestore, collection, doc, addDoc, getDocs, updateDoc, deleteDoc, query, where } from 'firebase/firestore';
 
 export const addUser = async (user: User) => {
-  const docRef = await db.collection('users').add(user);
+  const docRef = await addDoc(collection(db, 'users'), user);
+
   return docRef.id;
 };
 
@@ -11,15 +13,15 @@ export const getUser = async (userid: string) => {
 };
 
 export const addPost = async (post: Post) => {
-  const docRef = await db.collection('posts').add(post);
+  const docRef = await addDoc(collection(db, 'posts'), post)
   return docRef.id;
 };
 
 export const updateUser = async (userid: string, newData: Partial<User>): Promise<void> => {
-  const docRef = db.collection('users').doc(userid);
+  const userRef = doc(db, 'users', userid);
 
   try {
-    await docRef.update(newData);
+    await updateDoc(userRef, newData);
   } catch (error) {
     console.error("Error updating user:", error);
     throw error;
@@ -27,17 +29,18 @@ export const updateUser = async (userid: string, newData: Partial<User>): Promis
 };
 
 export const deletePost = async (postid: string) => {
-  const docRef = db.collection('posts').doc(postid);
+  const postRef = doc(db, 'posts', postid);
 
   try {
-    await docRef.delete();
+    await deleteDoc(postRef);
   } catch (error) {
     console.error("Error deleting post:", error);
   }
-}
+};
 
 export const filterPostsByCourse = async (course: string): Promise<Post[]> => {
-  const snapshot = await db.collection('posts').where('course', '==', course).get();
+  const postsQuery = query(collection(db, 'posts'), where('course', '==', course));
+  const snapshot = await getDocs(postsQuery);
   const filteredCourses: Post[] = [];
   snapshot.forEach((doc) => {
     filteredCourses.push(doc.data() as Post);
@@ -46,14 +49,14 @@ export const filterPostsByCourse = async (course: string): Promise<Post[]> => {
 };
 
 export const filterPostsByAvailability = async (course: string, availability: string[]): Promise<Post[]> => {
-  const snapshot = await db.collection('posts').where('course', '==', course).get();
+  const postsQuery = query(collection(db, 'posts'), where('course', '==', course));
+  const snapshot = await getDocs(postsQuery);
   const filteredPosts: Post[] = [];
   snapshot.forEach((doc) => {
     const postData = doc.data() as Post;
-    if (availability.some((avail) => postData.availabilities.includes(avail))) {
+    if (availability.some((avail) => postData.availabilities?.includes(avail))) {
       filteredPosts.push(postData);
     }
   });
   return filteredPosts;
 };
-
